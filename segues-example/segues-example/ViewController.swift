2c02a2e8de9548b529d56c451e0c1269c3952f97
Bruno Uthman <bruno.uthman@hotmail.com>
2019-02-14 19:43:15 -0200

    commit initial project segues-example completed - page navigation

---
+++ segues-example/segues-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  segues-example
+//
+//  Created by Bruno Uthman on 14/02/2019.
+//  Copyright © 2019 Bruno Uthman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
